290e3191ec7e36720da6476db8e6c0e1e2f23605
Jan Kuehle <jkuehle90@gmail.com>
2017-06-08 18:57:01 +0200

    Add sw-precache-config.js.

---
+++ src/sw-precache-config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+'use strict';
+
+module.exports = {
+    staticFileGlobs: [
+        'index.html',
+        'bower_components/webcomponentsjs/webcomponents-*.js',
+        'images/*'
+    ],
+    navigateFallback: 'index.html'
+};
