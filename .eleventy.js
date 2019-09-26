2b4cb2c5a2dd4fd741eb04fbbf68cd0406c1ac42
Jan Kuehle <jkuehle90@gmail.com>
2019-09-26 20:08:09 +0100

    Production build; extract config; cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const { join: joinPath } = require("path");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginFormatDate = require("./lib/11ty-format-date");
@@ -5,6 +6,7 @@ const pluginHtmlMin = require("./lib/11ty-htmlmin");
 const pluginImage = require("./lib/11ty-image");
 const pluginIntoFile = require("./lib/11ty-into-file");
 const pluginSubHeading = require("./lib/11ty-sub-heading");
+const { inputDir, outputDir } = require("./lib/utils/config");
 
 module.exports = eleventyConfig => {
 	eleventyConfig.addPlugin(pluginFormatDate);
@@ -15,13 +17,13 @@ module.exports = eleventyConfig => {
 	eleventyConfig.addPlugin(pluginSyntaxHighlight);
 	eleventyConfig.addPlugin(pluginRss);
 
-	eleventyConfig.addPassthroughCopy("**/.htaccess");
-	eleventyConfig.addPassthroughCopy("src/favicon.ico");
+	eleventyConfig.addPassthroughCopy(joinPath(inputDir, "**/.htaccess"));
+	eleventyConfig.addPassthroughCopy(joinPath(inputDir, "favicon.ico"));
 
 	return {
 		dir: {
-			input: "src",
-			output: "build"
+			input: inputDir,
+			output: outputDir
 		},
 		htmlTemplateEngine: "njk"
 	};
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # kuehle.me
 
 This is the source of my private website [kuehle.me](https://kuehle.me).
+
+## Usage
+
+For a development server with live reload:
+
+```sh
+yarn
+yarn start
+```
+
+For a production build:
+
+```sh
+NODE_ENV=production yarn build
+```
+
+To test the Apache config:
+
+```sh
+./docker/run.sh
+```
