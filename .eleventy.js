dd56f58132ddd4b5272c171ae6c917a11768a341
Jan Kuehle <jkuehle90@gmail.com>
2019-10-05 22:17:17 +0100

    Watch for CSS changes

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,6 @@ const markdownItAnchor = require("markdown-it-anchor");
 const pluginFormatDate = require("./lib/11ty-format-date");
 const pluginHtmlImages = require("./lib/11ty-html-images");
 const pluginHtmlMin = require("./lib/11ty-htmlmin");
-// const pluginImage = require("./lib/11ty-image");
 const pluginIntoFile = require("./lib/11ty-into-file");
 const pluginSubHeading = require("./lib/11ty-sub-heading");
 const { inputDir, outputDir } = require("./lib/utils/config");
@@ -15,7 +14,6 @@ module.exports = eleventyConfig => {
 	eleventyConfig.addPlugin(pluginFormatDate);
 	eleventyConfig.addPlugin(pluginHtmlImages);
 	eleventyConfig.addPlugin(pluginHtmlMin);
-	// eleventyConfig.addPlugin(pluginImage);
 	eleventyConfig.addPlugin(pluginIntoFile);
 	eleventyConfig.addPlugin(pluginSubHeading);
 	eleventyConfig.addPlugin(pluginSyntaxHighlight);
