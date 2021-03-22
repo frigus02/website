b78feebca82924239daa74c12b2da1b0226db0cf
Jan Kuehle <jkuehle90@gmail.com>
2021-03-22 19:45:43 +0100

    Don't push images for PR builds

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 IMAGE=frigus02/website
@@ -12,7 +12,9 @@ docker build -t "$IMAGE:$TAG" .
 docker tag "$IMAGE:$TAG" "$IMAGE:latest"
 
 if [ "$TAG" != "dev" ]; then
-    docker login -u frigus02 -p "$DOCKER_PASSWORD"
-    docker push "$IMAGE:$TAG"
-    docker push "$IMAGE:latest"
+	if [ -z ${CI_REF+x} ] || [ "${CI_REF-x}" == "refs/heads/main" ]; then
+		docker login -u frigus02 -p "$DOCKER_PASSWORD"
+		docker push "$IMAGE:$TAG"
+		docker push "$IMAGE:latest"
+	fi
 fi
